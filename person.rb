11c03cc8a70efac63b6c725c4632c378365fb46b
Daniel Falcon <Danfall963@Gmail.com>
2023-06-23 14:41:23 -0400

    adding asociations with book, classroom and rental

--- person.rb
+++ person.rb
@@ -1,30 +1,37 @@
 require_relative './nameable'
 
 class Person < Nameable
+  attr_accessor :name, :age, :rentals, :parent_permission
   attr_reader :id
-  attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name, parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  private
 
   def of_age?
-    @age >= 18
-  end
+    return true if @age >= 18
 
-  public
+    false
+  end
+  private :of_age?
 
   def can_use_services?
-    of_age? || @parent_permission
+    return true if of_age? || @parent_permission
+
+    false
   end
 
-  def correct_name
-    name
+  # method that returns name
+  def correct_name?
+    @name
+  end
+
+  def add_rental(rental)
+    rentals << rental unless rentals.include?(rental)
   end
 end
